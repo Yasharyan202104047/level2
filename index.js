819ea228ef6297c193b0fff018837b7d790d56de
Yasharyan202104047 <113341439+Yasharyan202104047@users.noreply.github.com>
2022-11-11 15:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ fs.readFile("project.html", (err, project) => {
   }
   projectContent = project;
 });
-fs.readFile("project.html", (err, project) => {
+fs.readFile("registration.html", (err, project) => {
   if (err) {
     throw err;
   }
